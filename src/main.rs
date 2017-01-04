9645063af11c788a0de7b4b22971f67e86e7715b
Lukas Andreas Muller <lamuller@protonmail.com>
2017-01-04 09:54:53 -0800

    Add basic capabilities

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std::fs::File;
+use std::error::Error;
+
+// MODELLING
+
+struct Item {
+    content: String
+} impl Item {
+    fn new(content: String) -> Item {
+        Item { content: content }
+    }
+}
+
+struct List {
+    name: String,
+    items: Vec<Item>
+} impl List {
+    fn new(name: &str, items: Vec<Item>) -> List {
+        List {
+            name: name.to_string(),
+            items: items
+        }
+    }
+    fn save(&self) {
+        create_file(&self.name, "txt")
+    }
+}
+
+// BASIC METHODS
+
+fn create_file(file_name: &str, file_extension: &str) {
+    let file = match File::create(format!("{}.{}", file_name, file_extension)) {
+        Ok(file) => file,
+        Err(why) => panic!("An error occured: {}", why.description())
+    };
+}
+
+fn open_file(file_name: &str, file_extension: &str) {
+    let file = match File::open(format!("{}.{}", file_name, file_extension)) {
+        Ok(file) => file,
+        Err(why) => panic!("An error occured: {}", why.description())
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    let list = List::new("example", vec![]);
+    list.save();
 }
